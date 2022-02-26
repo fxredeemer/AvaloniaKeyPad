e1ff97aa748ae9e0d5de5e55f4f795ecaffb0ff4
fxredeemer <flufeu@gmail.com>
2022-02-26 17:03:31 +0100

    binding keypress to command

--- AvaloniaKeyPad/ViewModels/Actions/ButtonActionViewModel.cs
+++ AvaloniaKeyPad/ViewModels/Actions/ButtonActionViewModel.cs
@@ -1,11 +1,54 @@
-﻿namespace AvaloniaKeyPad.ViewModels.Actions
+﻿using DynamicData.Binding;
+using ReactiveUI;
+using System;
+using System.Windows.Input;
+
+namespace AvaloniaKeyPad.ViewModels.Actions
 {
     public interface IButtonActionViewModel : IActionViewModel
     {
-
+        public string ButtonText { get; }
+        public IObservableCollection<string> PressedButtons { get; }
+        public ICommand ListenKeyPress { get; }
     }
 
     public class ButtonActionViewModel : ViewModelBase, IButtonActionViewModel
     {
+        private bool isRecording;
+
+        public string ButtonText => isRecording ? "Stop" : "Record";
+
+        public bool IsRecording
+        {
+            get => isRecording;
+            set => this.RaiseAndSetIfChanged(ref isRecording, value);
+        }
+
+        public IObservableCollection<string> PressedButtons { get; } = new ObservableCollectionExtended<string>();
+
+        public ICommand ListenKeyPress { get; }
+
+        public ButtonActionViewModel()
+        {
+            ListenKeyPress = ReactiveCommand.Create(ListenToKeypress);
+
+            this.WhenAnyValue(d => d.IsRecording)
+                .ToProperty(this, nameof(ButtonText));
+        }
+
+        public bool CanToggleRecording() => !isRecording;
+        public void ToggleRecording()
+        {
+            IsRecording = !IsRecording;
+        }
+
+        private void ListenToKeypress()
+        {
+            if (isRecording)
+            {
+
+
+            }
+        }
     }
 }
